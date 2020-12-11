26910c7d966448255aa577e09178205d16e54a6f
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-12-11 15:47:25 -0500

    when-i-walk video added to modal on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { Button, Jumbotron } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Jumbotron, Modal } from 'react-bootstrap';
 import './home.css';
 
 const Home = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return (
 		<div>
 			<Jumbotron id='build'>
+				<Modal show={show} onHide={handleClose} id='modal'>
+					<Modal.Header id='walk-with-me'>
+						<Modal.Title>When I Walk</Modal.Title>
+					</Modal.Header>
+					<Modal.Body id='video-container'>
+						<video
+							width='100%'
+							controls
+							src={process.env.PUBLIC_URL + '/media/when_i_walk.mp4'}></video>
+					</Modal.Body>
+					<Modal.Footer id='walk-with-me'>
+						<Button variant='secondary' onClick={handleClose}>
+							Close
+						</Button>
+					</Modal.Footer>
+				</Modal>
 				<p>
-					<Button variant='primary'>Learn more</Button>
+					<Button variant='primary' onClick={handleShow}>
+						Play
+					</Button>
 				</p>
 			</Jumbotron>
 		</div>
