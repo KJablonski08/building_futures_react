833dfe0a027dbe79388972871592b88667b6e558
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-12-30 22:38:15 -0500

    added projects folder to begin projects section - transformations

---
+++ src/components/Projects/Project.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Project = () => {
+	return (
+		<div>
+			<div>
+				When we began walking this path, we never imagined how far our feet
+				would carry us. It’s difficult to describe the transformations we
+				facilitate, so we thought it would be easier to simply show you. Whether
+				we are reconstructing classrooms or building a new facility, the
+				transformations are always remarkable. These are just a few of the
+				examples that show the contrasts between what we find and what we are
+				able to do with it. We do this work together. These projects are always
+				a group effort that combines laborers in Kenya, Kenyan suppliers, BFI
+				members, and the prayers, well-wishes and donations of thousands of
+				individuals. These changes cannot happen without people like you!
+			</div>
+		</div>
+	);
+};
+
+export default Project;
